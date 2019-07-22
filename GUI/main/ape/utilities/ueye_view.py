bbcf1721c3c40b2c28f71ca4c14cd4c30724705a
Alexander Rössler <mail@roessler.systems>
2019-07-22 21:29:13 +0200

    install_scripts: add dependencies

---
+++ GUI/main/ape/utilities/ueye_view.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from qtpy.QtCore import QObject
+
+
+class UEyeView(QObject):
+    def __init__(self, parent=None):
+        super(UEyeView, self).__init__(parent)
---
+++ install_scripts/docker/3-deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+WANT_ENV="docker-build docker-run"
+. $(dirname $0)/env.sh
+
+# Ensure apt cache is up to date
+apt-get update
+
+###########################
+# Tools
+###########################
+
+pip install \
+    opencv-python \
+    pyueye
+
+###########################
+# Clean up
+###########################
+apt-get clean
+if test_environment docker-build; then
+    rm -rf /root/.ccache
+fi
