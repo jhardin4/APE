2159422d65439cda08115631c442b274a782e70b
Alexander Rössler <mail@roessler.systems>
2019-07-23 11:35:42 +0000

    AppTemplates/FromFile: fix typo

--- AppTemplates/FromFile.py
+++ AppTemplates/FromFile.py
@@ -5,8 +5,8 @@ def FromFile(apparatus, filename):
     with open(filename, 'r') as oldApp:
         oldAppData = json.load(oldApp)
     # Clear the proclog
-    apparatus.proglog = []
-    apparatus['proclog'] = apparatus.proglog
+    apparatus.proclog = []
+    apparatus['proclog'] = apparatus.proclog
     # Replace the current device and information
     apparatus['devices'] = oldAppData['devices']
     apparatus['information'] = oldAppData['information']
