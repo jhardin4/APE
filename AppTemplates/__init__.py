4f27ad6d83d2e07530a8cba619b09a3d1b8a7b07
Alexander Rössler <mail@roessler.systems>
2019-07-23 12:48:07 +0000

    AppTemplates: add empty template

--- AppTemplates/__init__.py
+++ AppTemplates/__init__.py
@@ -1,2 +1,3 @@
 from AppTemplates.FlexPrinterGUI import FlexPrinterGUI  # noqa: F401
 from AppTemplates.FromFile import FromFile  # noqa: F401
+from AppTemplates.Empty import Empty  # noqa: F401
--- GUI/TemplateGUIs/__init__.py
+++ GUI/TemplateGUIs/__init__.py
@@ -1,2 +1,3 @@
 from GUI.TemplateGUIs.FlexPrinterGUI import FlexPrinterGUI  # noqa: F401
 from GUI.TemplateGUIs.FromFile import FromFile  # noqa: F401
+from GUI.TemplateGUIs.Empty import Empty  # noqa: F401
