b8e85780dcfbd0892839b24869e9d828dc8c03e2
Alexander Rössler <mail@roessler.systems>
2019-07-25 14:21:11 +0000

    Procedures/SampleTray_XY_Setup: fix grid assignment

--- Procedures/SampleTray_XY_Setup.py
+++ Procedures/SampleTray_XY_Setup.py
@@ -114,6 +114,4 @@ class SampleTray_XY_Setup(Procedure):
                         'used': False,
                     }
                 )
-        self.apparatus.setValue(
-            ['information', 'ProcedureData', self.name, 'trays', trayname], grid
-        )
+        self.apparatus.setValue(['information', 'trays', trayname], grid)
