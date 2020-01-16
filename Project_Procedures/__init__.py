1fe5788734d2d0bfb68e9b3f4a3e4e51d6ed735a
James Hardin <johardiniv@gmail.com>
2020-01-16 15:05:24 -0500

    Auto stash before merge of "jhardin" and "origin/jhardin"

---
+++ Project_Procedures/__init__.py
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+from Project_Procedures.FinalImages import FinalImages  # noqa: F401
+from Project_Procedures.InitialImages import InitialImages  # noqa: F401
+from Project_Procedures.SpanningSample import SpanningSample  # noqa: F401
+
+from Project_Procedures.OptSpeed import OptSpeed  # noqa: F401
+# from Project_Procedures.ImPredict import ImPredict  # noqa: F401
+# from Project_Procedures.Suggest import Suggest  # noqa: F401
+
+from Project_Procedures.PrintSample import PrintSample  # noqa: F401
+>>>>>>> Stashed changes
