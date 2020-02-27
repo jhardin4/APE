6d89504b355b6dab6bbfa89fd2e5f9826d2353e5
jhardin4 <johardiniv@gmail.com>
2020-02-27 15:07:37 -0500

    RoboDaddy Apparatus Builder
    Copied the FlexPrinter builder and updated it for RoboDaddy

--- AppTemplates/__init__.py
+++ AppTemplates/__init__.py
@@ -1,3 +1,4 @@
 from AppTemplates.FlexPrinterGUI import FlexPrinterGUI  # noqa: F401
 from AppTemplates.FromFile import FromFile  # noqa: F401
 from AppTemplates.Empty import Empty  # noqa: F401
+from AppTemplates.RoboDaddyMonolith import RoboDaddyMonolith
