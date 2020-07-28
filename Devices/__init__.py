0d52c93a4522b594a31dedaf83d8f8d76695e61e
James Hardin <johardiniv@gmail.com>
2020-07-28 12:33:12 -0400

    roseda device added

--- Devices/__init__.py
+++ Devices/__init__.py
@@ -12,3 +12,4 @@ from Devices.Nordson_UltimusV_A3200 import Nordson_UltimusV_A3200  # noqa: F401
 from Devices.Keyence_GT2_A3200 import Keyence_GT2_A3200  # noqa: F401
 from Devices.User_Consol import User_Consol  # noqa: F401
 from Devices.User_GUI import User_GUI  # noqa: F401
+from Devices.Roseda import Roseda
